e323d206a08433dd668d49f44813516c0518c0e7
Ashley Chatman <ashchat2008@gmail.com>
2022-12-13 12:54:00 -0500

    Prepare files -Add all files -Add comments

---
+++ sketch.js
@@ -0,0 +1,7 @@
+/*Create  with a 16 x 16 grid of square divs.
+  --Divs must be created with JavaScript
+  --The 16 x 16 should be in  a another provide by html
+  --Be able to change the size of grid on a click of a button
+    up to 64 x 64.
+  --Change the color from black and white to a random RGB color
+*/
