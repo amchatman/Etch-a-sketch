1ed1365ed10d4edbc37c80ee0a4f88b45563d8e3
Ashley Chatman <ashchat2008@gmail.com>
2022-12-15 14:30:19 -0500

    Range Fucnction -Update range input -Add function

--- sketch.js
+++ sketch.js
@@ -1,21 +1,19 @@
-/*Create  with a 16 x 16 grid of square divs.
-  --Divs must be created with JavaScript
-  --The 16 x 16 should be in  a another provide by html
-  --Be able to change the size of grid on a click of a button
-    up to 64 x 64.
-  --Change the color from black and white to a random RGB color
+/*--Change the color from black and white to a random RGB color
+--Create a function that populate the size of the board
 */
 const canvasBoard = document.querySelector('#canvasBoard');
 
-//Creates 16 columns
-canvasBoard.style.gridTemplateColumns = "repeat(16, 1fr)";
-//Creates 16 Rows
-canvasBoard.style.gridTemplateRows = "repeat(16, 1fr)";
-
-//16 x 16 = 256 
-for(i = 0; i < 256; i++){
-  const gridCanvas = document.createElement('div');
-  gridCanvas.style.backgroundColor = "green";
-  canvasBoard.insertAdjacentElement('beforeend',gridCanvas);
- 
+function populateCanvas(size){
+  //Creates 16 columns
+  canvasBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  //Creates 16 rows
+  canvasBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  //16 x 16 = 256 
+  for(i = 0; i < 256; i++){
+  //Create a div 
+    const gridCanvas = document.createElement('div');
+    gridCanvas.style.backgroundColor = "green";
+    canvasBoard.insertAdjacentElement('beforeend',gridCanvas);
+  }
 }
+populateCanvas(16);
