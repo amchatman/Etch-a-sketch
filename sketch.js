681165b2868a5f482e6a9603fa6dfe12166d2b10
Ashley Chatman <ashchat2008@gmail.com>
2022-12-16 13:26:41 -0500

    Canvas button -Correct for loop -Add changeCanvas function

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 /*--Change the color from black and white to a random RGB color
 --Create a function that populate the size of the board
 */
-const canvasBoard = document.querySelector('#canvasBoard');
 
 function populateCanvas(size){
+  const canvasBoard = document.querySelector('#canvasBoard');
   //Creates 16 columns
   canvasBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   //Creates 16 rows
   canvasBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
   //16 x 16 = 256 
-  for(i = 0; i < 256; i++){
+  for(let i = 0; i < 256; i++){
   //Create a div 
     const gridCanvas = document.createElement('div');
     gridCanvas.style.backgroundColor = "green";
@@ -17,3 +18,8 @@ function populateCanvas(size){
   }
 }
 populateCanvas(16);
+
+//Change the canvas size base on the input
+function changeCanvas(input){
+    populateCanvas(input);
+}
