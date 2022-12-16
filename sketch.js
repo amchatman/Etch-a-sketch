c13f6d92dccd35086167362a6a4b71b251055753
Ashley Chatman <ashchat2008@gmail.com>
2022-12-16 16:14:35 -0500

    Color function -Update button -Delete unused comment -Create changeColor and colorCanvas function

--- sketch.js
+++ sketch.js
@@ -1,19 +1,26 @@
-/*--Change the color from black and white to a random RGB color
---Create a function that populate the size of the board
-*/
+let color = "black";
 
 function populateCanvas(size){
   const canvasBoard = document.querySelector('#canvasBoard');
+
   //Creates 16 columns
   canvasBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
   //Creates 16 rows
   canvasBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+  const gridSize = size * size;
+
   //16 x 16 = 256 
-  for(let i = 0; i < 256; i++){
-  //Create a div 
+  for(let i = 0; i < gridSize; i++){
+
+    //Create a div 
     const gridCanvas = document.createElement('div');
-    gridCanvas.style.backgroundColor = "green";
+
+    //Create add listener on mouseover call colorCanvas function
+    gridCanvas.addEventListener("mouseover",colorCanvas);
+
+    gridCanvas.style.backgroundColor = "white";
     canvasBoard.insertAdjacentElement('beforeend',gridCanvas);
   }
 }
@@ -21,5 +28,21 @@ populateCanvas(16);
 
 //Change the canvas size base on the input
 function changeCanvas(input){
+
+  //Input must greater then 2 less then 100
+   if(input >= 2 && input <= 100){
     populateCanvas(input);
+   }else{
+    console.log('Too many squares')
+   }
+}
+//Set the color of the mouseover 
+function colorCanvas(){
+  this.style.backgroundColor = color;
+}
+
+//Change color
+function changeColor(choice){
+  color = choice;
+
 }
